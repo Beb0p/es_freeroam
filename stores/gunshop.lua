1ba5e0023bf063371e33abbdf83813dc8b63c840
Sith <sevendoctorslol@gmail.com>
2017-04-10 17:46:50 -0700

    Nightly build
    General code cleanup

--- stores/gunshop.lua
+++ stores/gunshop.lua
@@ -16,8 +16,10 @@ local gunshop_locations = {
 	{-320.156,6083.840,31.454-1.15},
 	{21.840,-1107.430,29.2797-0.50}
 }
-local gunshop_blips ={}
+
 local inrangeofgunshop = false
+
+local backlock = false
 
 -- menu
 
@@ -90,7 +92,7 @@ local function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
 end
 
 local function OpenCreator()		
-	local ped = LocalPed()
+	local ped = CurrentPed()
 	FreezeEntityPosition(ped,true)
 	gshp.currentmenu = "main"
 	gshp.opened = true
@@ -98,7 +100,7 @@ local function OpenCreator()
 end
 
 local function CloseCreator()
-	local ped = LocalPed()
+	local ped = CurrentPed()
 	FreezeEntityPosition(ped,false)
 	--table.sort(gshp.menu['main'].buttons)
 	gshp.opened = false
@@ -127,7 +129,7 @@ local function drawMenuButton(button,x,y,selected)
 	DrawText(x - menu.width/2 + 0.005, y - menu.height/2 + 0.0028)	
 end
 
-local function drawMenuInfo(text)
+local function drwMUI(text)
 	local menu = gshp.menu
 	SetTextFont(menu.font)
 	SetTextProportional(0)
@@ -140,7 +142,7 @@ local function drawMenuInfo(text)
 	DrawText(0.365, 0.934)	
 end
 
-local function drawMenuRight(txt,x,y,selected)
+local function drwMUIR(txt,x,y,selected)
 	local menu = gshp.menu
 	SetTextFont(menu.font)
 	SetTextProportional(0)
@@ -157,7 +159,7 @@ local function drawMenuRight(txt,x,y,selected)
 	DrawText(x + menu.width/2 - 0.03, y - menu.height/2 + 0.0028)	
 end
 
-local function drawMenuTitle(txt,x,y)
+local function drwMUITit(txt,x,y)
 local menu = gshp.menu
 	SetTextFont(2)
 	SetTextProportional(0)
@@ -169,7 +171,7 @@ local menu = gshp.menu
 	DrawText(x - menu.width/2 + 0.005, y - menu.height/2 + 0.0028)	
 end
 
-local function OpenMenu(menu)
+local function initMenu(menu)
 	gshp.lastmenu = gshp.currentmenu
 	if menu == "race" then
 		gshp.lastmenu = "main"
@@ -185,29 +187,29 @@ local function OpenMenu(menu)
 	gshp.selectedbutton = 0
 	gshp.currentmenu = menu	
 end
-local function ButtonSelected(button)
+local function menuSelection(button)
 	local ped = GetPlayerPed(-1)
 	local this = gshp.currentmenu
 	local btn = button.name
 	if this == "main" then
 		if btn == "Weapons" then
-			OpenMenu("weapons")
+			initMenu("weapons")
 		elseif btn == "Armor" then
-			OpenMenu("armor")
+			initMenu("armor")
 	elseif this == "weapons" then
-		if HasPedGotWeapon(LocalPed(),GetHashKey(button.model),false) then
-			AddAmmoToPed(LocalPed(),GetHashKey(button.model),GetMaxAmmoInClip(GetPlayerPed(-1),GetHashKey(button.model),1))
+		if HasPedGotWeapon(CurrentPed(),GetHashKey(button.model),false) then
+			AddAmmoToPed(CurrentPed(),GetHashKey(button.model),GetMaxAmmoInClip(GetPlayerPed(-1),GetHashKey(button.model),1))
 			-- Needs to have the money system hooked up
 		else
-			GiveWeaponToPed(LocalPed(), GetHashKey(button.model), 500, true, true)
+			GiveWeaponToPed(CurrentPed(), GetHashKey(button.model), 500, true, true)
 			-- Needs to have the money system hooked up
 		end
 		if GetMaxAmmoInClip(GetPlayerPed(-1),GetHashKey(button.model),1) ~= false then
 		table.insert(gshp.menu['selected_weapon'].buttons,{name = "Ammo", costs = 50, description = "", model = button.model})
-		OpenMenu('selected_weapon')
+		initMenu('selected_weapon')
 		end
 	elseif this == "selected_weapon" then
-		if btn == "Ammo" and Citizen.InvokeNative(0xDC16122C7A20C933,LocalPed(),GetHashKey(button.model),Citizen.PointerValueInt()) ~= GetAmmoInPedWeapon(LocalPed(),GetHashKey(button.model)) then
+		if btn == "Ammo" and Citizen.InvokeNative(0xDC16122C7A20C933,CurrentPed(),GetHashKey(button.model),Citizen.PointerValueInt()) ~= GetAmmoInPedWeapon(CurrentPed(),GetHashKey(button.model)) then
 			-- Needs to have the money system hooked up
 		end
 	elseif this == "armor" then
@@ -216,8 +218,6 @@ local function ButtonSelected(button)
 		end
 	end
 end
-
-local backlock = false
 
 local function Back()
 	if backlock then
@@ -229,9 +229,9 @@ local function Back()
 	elseif gshp.currentmenu == "selected_weapon" then
 		gshp.menu['selected_weapon'].buttons = {}
 		DisplayAmmoThisFrame(false)
-		OpenMenu(gshp.lastmenu)
+		initMenu(gshp.lastmenu)
 	else
-		OpenMenu(gshp.lastmenu)
+		initMenu(gshp.lastmenu)
 	end
 	
 end
@@ -243,14 +243,14 @@ Citizen.CreateThread( function()
 		Citizen.Wait(0)
 		local inrange = false
 		for i,b in ipairs(gunshop_locations) do
-			if IsPlayerWantedLevelGreater(GetPlayerIndex(),0) == false and  gshp.opened == false and IsPedInAnyVehicle(LocalPed(), true) == false and GetDistanceBetweenCoords(b[1],b[2],b[3],GetEntityCoords(LocalPed())) < 2 then
+			if IsPlayerWantedLevelGreater(GetPlayerIndex(),0) == false and  gshp.opened == false and IsPedInAnyVehicle(CurrentPed(), true) == false and GetDistanceBetweenCoords(b[1],b[2],b[3],GetEntityCoords(CurrentPed())) < 2 then
 				DrawMarker(1,b[1],b[2],b[3],0,0,0,0,0,0,1.501,1.5001,0.5001,0,155,255,200,0,0,0,0)
 				drawTxt('Press ~y~ENTER~s~ to ~g~purchase ~b~weapons',0,1,0.5,0.8,0.6,255,255,255,255)
 				inrange = true
 			end
 		end
 		inrangeofgunshop = inrange
-		if IsControlJustPressed(1,201) and IsPlayerInRangeOfgunshop() then
+		if IsControlJustPressed(1,201) and playerIsAtGunShop() then
 			if gshp.opened then
 				
 				CloseCreator()
@@ -259,10 +259,10 @@ Citizen.CreateThread( function()
 			end
 		end
 		if gshp.opened then
-			local ped = LocalPed()
+			local ped = CurrentPed()
 			local menu = gshp.menu[gshp.currentmenu]
 			drawTxt(gshp.title,1,1,gshp.menu.x,gshp.menu.y,1.0, 255,255,255,255)
-			drawMenuTitle(menu.title, gshp.menu.x,gshp.menu.y + 0.08)
+			drwMUITit(menu.title, gshp.menu.x,gshp.menu.y + 0.08)
 			drawTxt(gshp.selectedbutton.."/"..tablelength(menu.buttons),0,0,gshp.menu.x + gshp.menu.width/2 - 0.0385,gshp.menu.y + 0.067,0.4, 255,255,255,255)
 			local y = gshp.menu.y + 0.12
 			buttoncount = tablelength(menu.buttons)
@@ -279,24 +279,24 @@ Citizen.CreateThread( function()
 					drawMenuButton(button,gshp.menu.x,y,selected)
 					if button.costs ~= nil then
 						if gshp.currentmenu == "weapons" then
-							if HasPedGotWeapon(LocalPed(),GetHashKey(button.model),false) then
-								drawMenuRight("OWNED",gshp.menu.x,y,selected)
+							if HasPedGotWeapon(CurrentPed(),GetHashKey(button.model),false) then
+								drwMUIR("OWNED",gshp.menu.x,y,selected)
 							else
-								drawMenuRight(button.costs.."$",gshp.menu.x,y,selected)
+								drwMUIR(button.costs.."$",gshp.menu.x,y,selected)
 							end
 						elseif gshp.currentmenu == "selected_weapon" then
-							if Citizen.InvokeNative(0xDC16122C7A20C933,LocalPed(),GetHashKey(button.model),Citizen.PointerValueInt()) == GetAmmoInPedWeapon(LocalPed(),GetHashKey(button.model)) then
-								drawMenuRight("FULL",gshp.menu.x,y,selected)
+							if Citizen.InvokeNative(0xDC16122C7A20C933,CurrentPed(),GetHashKey(button.model),Citizen.PointerValueInt()) == GetAmmoInPedWeapon(CurrentPed(),GetHashKey(button.model)) then
+								drwMUIR("FULL",gshp.menu.x,y,selected)
 							else
-								drawMenuRight(button.costs.."$",gshp.menu.x,y,selected)
+								drwMUIR(button.costs.."$",gshp.menu.x,y,selected)
 							end
 						else
-							drawMenuRight(button.costs.."$",gshp.menu.x,y,selected)
+							drwMUIR(button.costs.."$",gshp.menu.x,y,selected)
 						end
 					end
 					y = y + 0.04
 					if selected and IsControlJustPressed(1,201) then
-						ButtonSelected(button)
+						menuSelection(button)
 					end
 				end
 			end	
@@ -331,80 +331,47 @@ Citizen.CreateThread( function()
 	end
 end)
 
-local function IsPlayerInRangeOfgunshop()
-return inrangeofgunshop
-end
-
-local function ShowgunshopBlips(bool)
-	if #gunshop_blips > 0 then
-		for i,b in ipairs(gunshop_blips) do
-			if DoesBlipExist(b.blip) then
-				SetBlipAsMissionCreatorBlip(b.blip,false)
-				Citizen.InvokeNative(0x86A652570E5F25DD, Citizen.PointerValueIntInitialized(b.blip))
-			end
-		end
-		gunshop_blips = {}
-	end
-		for station,pos in pairs(gunshop_locations) do
-			--[[local blip = AddBlipForCoord(pos[1],pos[2],pos[3])
-			-- 60 58 137
-			SetBlipSprite(blip,110)
-			BeginTextCommandSetBlipName("STRING")
-			AddTextComponentString('Ammu-Nation')
-			EndTextCommandSetBlipName(blip)
-			SetBlipAsShortRange(blip,true)
-			SetBlipAsMissionCreatorBlip(blip,true)
-			table.insert(gunshop_blips, {blip = blip, pos = pos})]]
-			-- READ THIS:
-			-- If you are using ESSENTIAL_FREEROAM, the gunstore blips are already on the map
-		end
-end
-
-local function Notify(text)
-	SetNotificationTextEntry('STRING')
-	AddTextComponentString(text)
-	DrawNotification(false, false)
+local function playerIsAtGunShop()
+	return inrangeofgunshop
 end
 
 -- utilities
 
-local function LocalPed()
+local function CurrentPed()
 	return GetPlayerPed(-1)
 end
 
 local function stringstarts(String,Start)
-   return string.sub(String,1,string.len(Start))==Start
+	return string.sub(String,1,string.len(Start))==Start
 end
 
 local function tablelength(T)
-  local count = 0
-  for _ in pairs(T) do count = count + 1 end
-  return count
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
 end
 
 local function round(num, idp)
-  if idp and idp>0 then
-    local mult = 10^idp
-    return math.floor(num * mult + 0.5) / mult
-  end
-  return math.floor(num + 0.5)
+	if idp and idp>0 then
+	local mult = 10^idp
+		return math.floor(num * mult + 0.5) / mult
+	end
+	return math.floor(num + 0.5)
 end
 
 
 local function f(n)
-return n + 0.0001
-end
-
-local function LocalPed()
-return GetPlayerPed(-1)
+	return n + 0.0001
 end
 
 local function try(f, catch_f)
-local status, exception = pcall(f)
-if not status then
-catch_f(exception)
+	local status, exception = pcall(f)
+
+	if not status then
+		catch_f(exception)
+	end
 end
-end
+
 local function firstToUpper(str)
     return (str:gsub("^%l", string.upper))
 end
